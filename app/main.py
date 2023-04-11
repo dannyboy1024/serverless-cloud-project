2b245b9ec7f2aae15e55114d803d6f0d415c47ee
stephen-shichen <jsczj.canada@gmail.com>
2023-04-10 22:16:50 -0400

    Add S3 code

--- app/main.py
+++ app/main.py
@@ -29,6 +29,7 @@ def main():
 def upload():
     # upload image with key
     # transfer the bytes into dict
+    s3client = boto3.client('s3', region_name='us-east-1')
     data = request.form
     key = data.get('key')
     imageContent = data.get('file')
@@ -46,6 +47,11 @@ def upload():
     full_file_path = os.path.join(os_file_path, filename)
     # requests.post(memcache_pool_url + '/put', params=requestJson) TODO: AWS Cache service + S3
     # requests.post(webapp_url + '/uploadToDB', params=requestJson) TODO: dynamodb
+    if os.path.isfile(full_file_path):
+        os.remove(full_file_path)
+    with open(full_file_path, 'w') as fp:
+        fp.write(value)
+    s3client.upload_file(full_file_path, bucket_name, filename)
     db.insertFileInfo(tableName='fileInfo', fileInfo=FILEINFO(key, full_file_path, imageSize))
     
     resp = OrderedDict([("success", "true"), ("key", key)])
@@ -59,44 +65,46 @@ def upload():
 
 @webapp.route('/retrieve/<key_value>', methods=['GET', 'POST'])
 def retrieve(key_value):
+    s3client = boto3.client('s3', region_name='us-east-1')
     requestJson = {
         'key': key_value
     }
     # res = requests.post(webapp_url + '/getFromS3', params=requestJson) # TODO: S3 + DynamoDB
     res = None
-    fileInfo = db.readFileInfo('fileInfo', key_value)
-    if res.status_code == 400:
-        resp = OrderedDict()
-        resp["success"] = "false"
-        resp["error"] = {
-            "code": 400,
-            "message": "Target file is not found because the given key is not found."
-        }
+    # TODO: dynamodb
+    fileInfo = db.readFileInfo(key_value)
+    if fileInfo != None: 
+        filename = fileInfo.location
+        fileSize = fileInfo.size
+        checkFile = s3client.list_objects_v2(Bucket=bucket_name, Prefix=filename)
+        if "Contents" in checkFile:
+            full_file_path = os.path.join(os_file_path, filename)
+            s3client.download_file(bucket_name, filename, full_file_path)
+            value = Path(full_file_path).read_text()
+            resp = {
+                "success" : "true", 
+                "value": value, 
+                "size": fileSize
+            }
+            response = webapp.response_class(
+                response=json.dumps(resp),
+                status=200,
+                mimetype='application/json'
+            )
+        else:
+            response = webapp.response_class(
+                response=json.dumps("File Not Found"),
+                status=400,
+                mimetype='application/json'
+            )
+    else:
+        print("Not found in DB")
         response = webapp.response_class(
-            response=json.dumps(resp),
+            response=json.dumps("Not found in DB"),
             status=400,
             mimetype='application/json'
         )
-    else:
-        json_response = res.json()
-        value = json_response["value"]
-        size = json_response["size"]
-        content = base64.b64decode(value)
-        resp = OrderedDict()
-        resp["success"] = "true"
-        resp["key"] = key_value
-        resp["content"] = bytes.decode(content)
-        requestJson = {
-            'key': key_value,
-            'value': value, 
-            'size': size
-        }
-        # requests.post(memcache_pool_url + '/put', params=requestJson) TODO: AWS Cache service
-        response = webapp.response_class(
-            response=json.dumps(resp),
-            status=200,
-            mimetype='application/json'
-        )
+
     return response
 
 @webapp.route('/delete_all', methods=['POST'])
@@ -105,6 +113,7 @@ def delete_all():
     Remove all the key and values (files, images) from the database and filesystem
     No inputs required
     """
+    s3client = boto3.client('s3', region_name='us-east-1')
     response = s3client.list_objects_v2(Bucket=bucket_name)
     if 'Contents' in response: 
         for obj in response['Contents']:
