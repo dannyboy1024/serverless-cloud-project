5d6bf56c003ee70a75cbc41f94e49f162abf4e63
stephen-shichen <jsczj.canada@gmail.com>
2023-04-16 16:34:19 -0400

    Background success

--- background.py
+++ background.py
@@ -4,40 +4,43 @@ import psutil
 import time
 import json
 import boto3
-import math
 from datetime import datetime
-from config import Config
-
-
-#convert mins to datetime obj - not working as expected 
-def convert_unix_to_utc(unix_in_min):
-	ts = int(unix_in_min * 60)
-	return datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 # lambda client
-lambda_client = boto3.client('lambda')
+boto_session = boto3.Session(
+    aws_access_key_id = 'AKIAXHQE6ZG47G5N5PDN',
+    aws_secret_access_key = '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',
+    region_name='us-east-1'
+)
+
+lambda_client = boto_session.client('lambda')
+lambda_functions = lambda_client.list_functions()
+print(lambda_functions)
 
 # data collection
 # cpu_util, mem_util, disk_util, all in percentage
 while True:
-	time_stamp = math.floor(time.time() / 60)
+	now = datetime.now()
+	time_stamp = now.strftime("%Y/%m/%d, %H:%M:%S")
 	data = {}
 	cpu_util = psutil.cpu_percent(interval=None)
 	mem_util = psutil.virtual_memory()
 	mem_util = mem_util.percent
 	disk_util = psutil.disk_usage('/')
 	disk_util = disk_util.percent
-	data['timestamp'] = time_stamp
+	data['timestamp'] = str(time_stamp)
 	data['Metrics'] = {}
-	data['Metrics']['CPU_Usage'] = cpu_util
-	data['Metrics']['Mem_Usage'] = mem_util
-	data['Metrics']['Disk_Usage'] = disk_util
+	data['Metrics']['CPU_Usage'] = str(cpu_util)
+	data['Metrics']['Mem_Usage'] = str(mem_util)
+	data['Metrics']['Disk_Usage'] = str(disk_util)
 	json_data = json.dumps(data, sort_keys=True, default=str)
 
 	print(json_data)
 
 	# invoke lambda
-	response = lambda_client.invoke(FunctionName='uploadPMToDynamo', Payload= json_data)
+	response = lambda_client.invoke(FunctionName='uploadPMToDynamo', Payload=json_data)
+	results = response['Payload'].read()
 
 	print(response)
-	time.sleep(60)
+	print(results)
+	time.sleep(30)
